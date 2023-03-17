d48ae25cd27b7358b7c127d995b544da3d3e02cd
Phil Georgiou <philipposgeorgiou7@gmail.com>
2023-03-17 19:06:45 +0000

    Added filtering functionality

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+// Defining filters as key : value pairs
+// each value is a function we will use to filters the 'tasks' data array
+// these are defined globally on purpose. If defined within App.js, they would be recalculated every time the 
+// <App /> components re-renders. 
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed, 
+  Completed: (task) => task.completed
+}
+
+// grab reference to an array of FILTER_MAPs properties
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   
+  // add tasks hook
   const [tasks, setTasks] = useState(props.tasks);
+
+  // filter hook
+  const [filter, setFilter] = useState("All");
 
   function toggleTaskCompleted(id) {
     
@@ -28,7 +45,9 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
-  const taskList = tasks.map((task) => (
+  const taskList = tasks
+   .filter(FILTER_MAP[filter])
+   .map((task) => (
     <Todo name={task.name} 
      completed={task.completed}
      id={task.id}
@@ -38,6 +57,11 @@ function App(props) {
      editTask={editTask}
      />
   ));
+
+  // we map over FILTER_NAMES array
+  const filterList = FILTER_NAMES.map((name) => {
+    return <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter}/>
+  });
 
   const headingNoun = taskList.length !== 1 ? 'tasks' : 'task'; 
   const headingText = `${taskList.length} ${headingNoun} remaining`;
@@ -63,9 +87,7 @@ function App(props) {
       <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        { filterList }
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
