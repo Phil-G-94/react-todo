9144d5e08c0d28c625ba081583f3b501765c1e86
Phil Georgiou <philipposgeorgiou7@gmail.com>
2023-03-17 20:27:17 +0000

    Code formatted

--- src/App.js
+++ src/App.js
@@ -1,102 +1,109 @@
-import React from "react";
-import Todo from "./components/Todo";
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
-import { useState } from "react";
-import { nanoid } from "nanoid";
+import React from 'react';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 // Defining filters as key : value pairs
 // each value is a function we will use to filters the 'tasks' data array
-// these are defined globally on purpose. If defined within App.js, they would be recalculated every time the 
-// <App /> components re-renders. 
+// these are defined globally on purpose. If defined within App.js, they would be recalculated every time the
+// <App /> components re-renders.
 
 const FILTER_MAP = {
-  All: () => true,
-  Active: (task) => !task.completed, 
-  Completed: (task) => task.completed
-}
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+};
 
 // grab reference to an array of FILTER_MAPs properties
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  
-  // add tasks hook
-  const [tasks, setTasks] = useState(props.tasks);
+    // add tasks hook
+    const [tasks, setTasks] = useState(props.tasks);
 
-  // filter hook
-  const [filter, setFilter] = useState("All");
+    // filter hook
+    const [filter, setFilter] = useState('All');
 
-  function toggleTaskCompleted(id) {
-    
-    const updatedTasks = tasks.map((task) => {
-      if (id === task.id) {
-        return {...task, completed: !task.completed}
-      }
-      return task;
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            if (id === task.id) {
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+
+        setTasks(updatedTasks);
+        console.log(tasks);
+    }
+
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks);
+    }
+
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map((task) => (
+            <Todo
+                name={task.name}
+                completed={task.completed}
+                id={task.id}
+                key={task.id}
+                toggleTaskCompleted={toggleTaskCompleted}
+                deleteTask={deleteTask}
+                editTask={editTask}
+            />
+        ));
+
+    // we map over FILTER_NAMES array
+    const filterList = FILTER_NAMES.map((name) => {
+        return (
+            <FilterButton
+                key={name}
+                name={name}
+                isPressed={name === filter}
+                setFilter={setFilter}
+            />
+        );
     });
 
-    setTasks(updatedTasks);
-    console.log(tasks);
-  }
+    const headingNoun = taskList.length !== 1 ? 'tasks' : 'task';
+    const headingText = `${taskList.length} ${headingNoun} remaining`;
 
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
-    setTasks(remainingTasks);
-  }
-  
-  const taskList = tasks
-   .filter(FILTER_MAP[filter])
-   .map((task) => (
-    <Todo name={task.name} 
-     completed={task.completed}
-     id={task.id}
-     key={task.id}
-     toggleTaskCompleted={toggleTaskCompleted}
-     deleteTask={deleteTask} 
-     editTask={editTask}
-     />
-  ));
+    function addTask(name) {
+        const newTask = { name, id: `todo-${nanoid()}`, completed: false };
+        setTasks([...tasks, newTask]);
+    }
 
-  // we map over FILTER_NAMES array
-  const filterList = FILTER_NAMES.map((name) => {
-    return <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter}/>
-  });
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map((task) => {
+            if (id === task.id) {
+                return { ...task, name: newName };
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
+    }
 
-  const headingNoun = taskList.length !== 1 ? 'tasks' : 'task'; 
-  const headingText = `${taskList.length} ${headingNoun} remaining`;
+    return (
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <Form addTask={addTask} />
 
-  function addTask(name) {
-    const newTask = {name, id: `todo-${nanoid()}`,completed: false};
-    setTasks([...tasks, newTask]);
-  }
-
-  function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
-      if (id === task.id) {
-        return {...task, name: newName}
-      }
-      return task;
-    });
-    setTasks(editedTaskList);
-  }
-  
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form addTask={addTask} />
-
-      <div className="filters btn-group stack-exception">
-        { filterList }
-      </div>
-      <h2 id="list-heading">{headingText}</h2>
-      <ul
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-          {taskList}           
-      </ul>
-    </div>
-  );
+            <div className="filters btn-group stack-exception">
+                {filterList}
+            </div>
+            <h2 id="list-heading">{headingText}</h2>
+            <ul
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading"
+            >
+                {taskList}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const DATA = [
-  {name: "Eat", completed: false, id: "todo-0" },
-  {name: "Jits", completed: false, id: "todo-1" }, 
-  {name: "Game", completed: false, id: "todo-2"}, 
-  {name: "Repeat", completed: false, id: "todo-3"}
+    { name: 'Eat', completed: false, id: 'todo-0' },
+    { name: 'Jits', completed: false, id: 'todo-1' },
+    { name: 'Game', completed: false, id: 'todo-2' },
+    { name: 'Repeat', completed: false, id: 'todo-3' },
 ];
 
 root.render(
-  <React.StrictMode>
-    <App tasks={DATA} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App tasks={DATA} />
+    </React.StrictMode>
 );
